03d8825b6f678b8d20c6d4b38984802fd71ec677
Emiliakazoo <emiliakazoo@gmail.com>
2017-03-29 09:01:32 -0700

    Update layout, add data

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,31 @@
 $(document).foundation();
+
+
+// server side call?
+
+//var posts = [];	
+
+$(function(){
+
+  $.getJSON('https://www.googleapis.com/blogger/v3/blogs/5224594161388198627/posts?key=AIzaSyCBOeJOBn0l_c2dWR-eUCk5qZ6_K11u8gw', function(data){
+    showResults(data.items);
+    //console.log(data.items); 
+  });
+});
+
+function showResults(results){
+	var item = {};
+	var posts = [];
+	var result = "";
+	$.each(results, function(index, value){ 
+		//console.log(results[index]);
+		item = {
+			"content": results[index].content,
+			"title": results[index].title
+		};
+		result = "<li><h2>" + item.title + "</h2><div class='content'>" + item.content + "</div></li>"
+		$(".results").append(result);
+		posts.push(item);
+	});
+	return posts; 
+}
